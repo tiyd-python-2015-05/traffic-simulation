853e67d29fcc90eddfcfdfe831e3091205a74974
Josh Higgins <jshhggns@gmail.com>
2015-05-21 17:17:14 -0400

    Next step simulation.

--- car.py
+++ car.py
@@ -1,25 +1,63 @@
 __author__ = 'joshuahiggins'
+import random
 
 class Car:
     def __init__(self, speed=0):
         """Car has length 5 and speed 0 to start.  Can pass speed."""
         self.length = 5
         self.speed = speed
+        self.location = 0
+        self.decelerate_chance = 1
+        #self.accelerate_chance will be changed by the simulation for longer distances
 
 
     def accelerate(self):
         """When car accelerates, goes 2m/s faster next time."""
         self.speed += 2
+        if self.speed > 34:
+            self.speed = 34
 
 
     def decelerate(self):
         """When car decelerates, goes 2m/s slower next time."""
         self.speed -= 2
+        if self.speed < 0:
+            self.speed = 0
 
 
     def slow_down(self, other):
         """Car sets itself to other car's speed."""
         self.speed = other.speed
+
+
+    def calculate_slowdown(self, other):
+        """Returns true if car distance to next car is less than speed"""
+        #Need to work out location when it loops back
+        if ((other.location - 5) - self.location ) < 0:
+            if other.location + 995 - self.location < self.speed:
+                return True
+            else:
+                return False
+        elif ((other.location - 5) - self.location) < self.speed:
+            return True
+        else:
+            return False
+
+    def set_decelerate_chance(self):
+        """This should be for hard mode"""
+        pass
+
+    def set_speed(self, other):
+        """Sets car speed based on self.location and other.location"""
+        if self.calculate_slowdown(other):
+            self.slow_down(other)
+        elif random.random() <= (.1 * self.decelerate_chance):
+            self.decelerate()
+        else:
+            self.accelerate()
+
+
+
 
 
 
--- car_test.py
+++ car_test.py
@@ -6,9 +6,19 @@ def test_car_has_length():
     car = Car()
     assert car.length == 5
 
+def test_car_has_location():
+    car = Car()
+    assert car.location == 0
+
 def test_car_has_speed():
     car = Car()
     assert car.speed == 0
+
+def test_car_can_not_go_faster_than_34():
+    car = Car()
+    car.speed = 33
+    car.accelerate()
+    assert car.speed == 34
 
 def test_car_accelerates():
     car = Car()
@@ -21,6 +31,12 @@ def test_car_decelerates():
     car.decelerate()
     assert car.speed == 28
 
+def test_car_can_not_go_negative():
+    car = Car()
+    car.speed = 0
+    car.decelerate()
+    assert car.speed == 0
+
 def test_car_matches_speed_car_in_front():
     car1 = Car()
     car1.speed = 24
@@ -29,9 +45,48 @@ def test_car_matches_speed_car_in_front():
     car1.slow_down(car2)
     assert car1.speed == 20
 
-def test_car_checks_position_and_sets_next_speed(location_1, location_2):
-    """location 1 is the location of the current car, location 2 is the location
-    of the second car"""
+def test_car_checks_position_and_sets_next_speed():
+    car1 = Car()
+    car2 = Car()
+    car1.speed = 30
+    car2.speed = 25
+
+def test_car_decides_to_slow_down():
+    car1 = Car()
+    car2 = Car()
+    car1.speed = 25
+    car2.speed = 27
+    car1.location = 100
+    car2.location = 80
+    assert car2.calculate_slowdown(car1) == True
+
+def test_car_does_not_slow_down():
+    car1 = Car()
+    car2 = Car()
+    car1.speed = 25
+    car2.speed = 27
+    car1.location = 115
+    car2.location = 80
+    assert car2.calculate_slowdown(car1) == False
+
+def test_car_that_has_looped_still_makes_car_behind_slow_down():
+    car1 = Car()
+    car2 = Car()
+    car1.speed = 25
+    car2.speed = 27
+    car1.location = 2
+    car2.location = 980
+    assert car2.calculate_slowdown(car1) == True
+
+def test_car_that_has_looped_does_not_make_car_behind_slow_down():
+    car1 = Car()
+    car2 = Car()
+    car1.speed = 25
+    car2.speed = 27
+    car1.location = 15
+    car2.location = 980
+    assert car2.calculate_slowdown(car1) == False
+
 
 
 
