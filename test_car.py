f73b5ed7a18b9221f1001babe06cdb6cd927e1c3
John Waldrep <johnwaldrep@gmail.com>
2015-05-22 07:20:49 -0400

    Refactor car tests to take a road; 13/14 pass

--- test_car.py
+++ test_car.py
@@ -15,7 +15,8 @@ from nose.tools import raises
 """
 
 def test_car_creation():
-    car = Car()
+    road = Road()
+    car = Car(road)
     assert car.desired_speed == 120
     assert car.length == 5
     assert car.accel_rate == 2
@@ -26,7 +27,7 @@ def test_car_creation():
 
     assert car.desired_spacing == car.speed
 
-    car = Car(position=5, desired_speed=130, length=6, accel_rate=3,
+    car = Car(road, position=5, desired_speed=130, length=6, accel_rate=3,
                 slowing_chance=0.2, decel_rate=3, init_speed=61,
                 desired_spacing_factor=2)
     assert car.position == 5
@@ -41,47 +42,53 @@ def test_car_creation():
     assert car.desired_spacing == car.speed * 2
 
 def test_car_accelerate():
-    car = Car()
+    road = Road()
+    car = Car(road)
     assert car.speed == 60
     car.accelerate()
     assert car.speed == 62
 
 def test_car_stop():
-    car = Car()
+    road = Road()
+    car = Car(road)
     assert car.speed == 60
     car.stop()
     assert car.speed == 0
 
 def test_update_position():
-    car = Car(init_speed=100)
+    road = Road()
+    car = Car(road, init_speed=100)
     assert car.position == 0 and car.speed == 100
     car.update_position()
     assert car.position == 0 + car.m_per_s * car.s_per_step
 
 def test_brake_if_needed():
     # Stop if collision imminent
-    car1 = Car(position=200, init_speed=100)
-    car2 = Car(position=210)
+    road = Road()
+    car1 = Car(road, position=200, init_speed=100)
+    car2 = Car(road, position=210)
     did_brake = car1.brake_if_needed(car2)
     assert did_brake is True
     assert car1.speed == 0
 
 def test_match_speed():
-    car1 = Car(position=200, init_speed=100)
-    car2 = Car(position=230)
+    road = Road()
+    car1 = Car(road, position=200, init_speed=100)
+    car2 = Car(road, position=230)
     assert car2.speed == 60 != car1.speed
     car1.match_speed(car2)
     assert car1.speed == car2.speed == 60
 
-    car1 = Car(position=200, init_speed=100)
-    car2 = Car(position=240)
+    car1 = Car(road, position=200, init_speed=100)
+    car2 = Car(road, position=240)
     assert car2.speed == 60 != car1.speed
     car1.brake_if_needed(car2)
     assert car1.speed == car2.speed == 60
 
 def test_car_slows_if_over_desired_speed():
-    car1 = Car(init_speed=100, desired_speed=60)
-    car2 = Car(position=900)
+    road = Road()
+    car1 = Car(road, init_speed=100, desired_speed=60)
+    car2 = Car(road, position=900)
     with mock.patch("random.random", return_value=1):
         # disable random braking
         car1.brake_if_needed(car2)
@@ -97,7 +104,8 @@ def test_car_slows_if_over_desired_speed():
     assert car1.speed == 60
 
 def test_car_decelerate():
-    car = Car()
+    road = Road()
+    car = Car(road)
     assert car.speed == 60
     car.decelerate()
     assert car.speed == 58
@@ -107,27 +115,29 @@ def test_car_decelerate():
 
 def test_step():
     # Stop if collision imminent
-    car1 = Car(position=200, init_speed=100)
-    car2 = Car(position=210)
+    road = Road()
+    car1 = Car(road, position=200, init_speed=100)
+    car2 = Car(road, position=210)
     car1.step(car2)
     assert car1.speed == 0
 
-    car1 = Car(position=200, init_speed=100)
-    car2 = Car(position=240)
+    car1 = Car(road, position=200, init_speed=100)
+    car2 = Car(road, position=240)
     car1.step(car2)
     assert car1.speed == car2.speed
 
-    car1 = Car(position=200, init_speed=100)
-    car2 = Car(position=400)
+    car1 = Car(road, position=200, init_speed=100)
+    car2 = Car(road, position=400)
     with mock.patch("random.random", return_value=1):
         car1.step(car2)
     assert car1.speed == 102
 
 def test_accel_and_decel_are_mutually_exclusive():
+    road = Road()
     for _ in range(100):
         with mock.patch("random.random", return_value=1):
-            car1 = Car(position=200, init_speed=100)
-            car2 = Car(position=400)
+            car1 = Car(road, position=200, init_speed=100)
+            car2 = Car(road, position=400)
             car1.step(car2)
             assert car1.speed == 102 or car1.speed == 98
 
@@ -140,4 +150,5 @@ def test_car_is_validating_position_with_road():
     assert False
 
 # Add road with looping zero
-# Add pytest and nose to requirements.txr
+# Add pytest and nose to requirements.txt
+# What about the speed limit?
