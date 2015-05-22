fddd8f774b1b1316bd53827ceee99ce5a7138d12
John Waldrep <johnwaldrep@gmail.com>
2015-05-22 08:42:31 -0400

    Add initial Simulation class and test files

---
+++ simulation.py
@@ -0,0 +1,11 @@
+"""
+  Simulation:
+    Is passed in a set of cars X and a roadX
+    Advances the step count and polls each car's position, and
+      reports the next car's position to each car
+    Saves position of each car into a time x road position numpy array
+    Returns the numpy array after the appropriate number of steps
+"""
+
+class Simulation():
+    pass
---
+++ test_simulation.py
@@ -0,0 +1,14 @@
+from simulation import Simulation
+
+"""
+  Simulation:
+    Is passed in a set of cars X and a roadX
+    Advances the step count and polls each car's position, and
+      reports the next car's position to each car
+    Saves position of each car into a time x road position numpy array
+    Returns the numpy array after the appropriate number of steps
+"""
+
+def test_simulation_creation():
+    sim = Simulation()
+    assert False
