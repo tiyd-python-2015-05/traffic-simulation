44f94be40dd544ac476ad9f399ff6b0cfaaee7e1
John Waldrep <johnwaldrep@gmail.com>
2015-05-22 06:08:19 -0400

    Add tested Car behavior

--- car.py
+++ car.py
@@ -1,23 +1,29 @@
+import random
+from itertools import cycle
 
 """
 Responsibilities:
   Car:
     Must know about car in front's current position and speed
     Created with an accel rate, desired speed, size, min spacing,
-      slowing chance, length (assume 5m for now)
+      slowing chance, length (assume 5m for now), position
     At each time step, make decision whether to speed up, slow down, or stop,
       based on leading car's current position/speed, self's desired following distance,
       and self's current speed, desired speed, and slowing chance, and road conditions
+      THEN update position with new speed, etc
     Car stops if continuing will cause a collision (collisions not modeled)
     Ask the Road current road condition
     Ask the Road if current position is valid or has to turn over
 """
-
+class Road():
+    pass
+    # pass into car also
+    # responsible for looping x values
 
 class Car():
-    def __init__(self, desired_speed=120, length=5, accel_rate=2,
+    def __init__(self, position=0, desired_speed=120, length=5, accel_rate=2,
                 slowing_chance=0.1, decel_rate=2, init_speed=60,
-                desired_spacing_factor=1):
+                desired_spacing_factor=1, s_per_step=1):
         self.desired_speed = desired_speed
         self.length = length
         self.accel_rate = accel_rate
@@ -25,7 +31,64 @@ class Car():
         self.decel_rate = decel_rate
         self.speed = init_speed
         self.desired_spacing_factor = desired_spacing_factor
+        self.position = position
+        self.s_per_step = s_per_step
 
     @property
     def desired_spacing(self):
         return self.speed * self.desired_spacing_factor
+
+    @property
+    def m_per_s(self):
+        kmh_to_mps = 1000 / 3600  # (1000m/km) / (60s * 60m)
+        return self.speed * kmh_to_mps
+
+    def accelerate(self):
+        self.speed = self.speed + self.accel_rate
+        if self.speed > self.desired_speed:
+            self.speed = self.desired_speed
+
+    def stop(self):
+        self.speed = 0
+
+    def update_position(self):
+        self.position = self.position + self.m_per_s * self.s_per_step
+        return self.position
+
+    def match_speed(self, car2):
+        self.speed = car2.speed
+
+    def decelerate(self):
+        self.speed = self.speed - self.decel_rate * self.s_per_step
+        if self.speed < 0:
+            self.speed = 0
+
+    def brake_if_needed(self, car2):
+        rear_bumper = car2.position - car2.length
+        buffer_zone = rear_bumper - self.desired_spacing
+        potential_position = self.position + self.m_per_s * self.s_per_step
+        print('rear_bumper: {}, buffer_zone: {}, potential_position: {}'.format(
+              rear_bumper, buffer_zone, potential_position))
+        if self.speed > self.desired_speed:
+            self.speed = self.desired_speed
+
+        if potential_position >= rear_bumper:
+            self.stop()
+            return True
+        elif potential_position >= buffer_zone:
+            self.match_speed(car2)
+            return True
+        elif random.random() < self.slowing_chance:
+            self.decelerate()
+            return True
+        else:
+            return False
+
+    def step(self, car2):
+        did_brake = self.brake_if_needed(car2)
+        if not did_brake:
+            self.accelerate()
+        self.update_position()
+        return self.position, self.speed
+
+# brake if needed first, if not braked, then accelerate
--- test_car.py
+++ test_car.py
@@ -1,12 +1,11 @@
 from car import Car
+from unittest import mock
 
 """
-Responsibilities:
   Car:
     Must know about car in front's current position and speed
-    Created with an accel rate, desired speed, size, min spacing,
-      slowing chance, length (assume 5m for now)
-    At each time step, make decision whether to speed up, slow down, or stop,
+    At each time step, update position if possible
+      then make decision whether to speed up, slow down, or stop,
       based on leading car's current position/speed, self's desired following distance,
       and self's current speed, desired speed, and slowing chance, and road conditions
     Car stops if continuing will cause a collision (collisions not modeled)
@@ -26,9 +25,10 @@ def test_car_creation():
 
     assert car.desired_spacing == car.speed
 
-    car = Car(desired_speed=130, length=6, accel_rate=3,
+    car = Car(position=5, desired_speed=130, length=6, accel_rate=3,
                 slowing_chance=0.2, decel_rate=3, init_speed=61,
                 desired_spacing_factor=2)
+    assert car.position == 5
     assert car.desired_speed == 130
     assert car.length == 6
     assert car.accel_rate == 3
@@ -42,7 +42,7 @@ def test_car_creation():
 def test_car_accelerate():
     car = Car()
     assert car.speed == 60
-    car.accellerate()
+    car.accelerate()
     assert car.speed == 62
 
 def test_car_stop():
@@ -50,3 +50,77 @@ def test_car_stop():
     assert car.speed == 60
     car.stop()
     assert car.speed == 0
+
+def test_update_position():
+    car = Car(init_speed=100)
+    assert car.position == 0 and car.speed == 100
+    car.update_position()
+    assert car.position == 0 + car.m_per_s * car.s_per_step
+
+def test_brake_if_needed():
+    # Stop if collision imminent
+    car1 = Car(position=200, init_speed=100)
+    car2 = Car(position=210)
+    did_brake = car1.brake_if_needed(car2)
+    assert did_brake is True
+    assert car1.speed == 0
+
+def test_match_speed():
+    car1 = Car(position=200, init_speed=100)
+    car2 = Car(position=230)
+    assert car2.speed == 60 != car1.speed
+    car1.match_speed(car2)
+    assert car1.speed == car2.speed == 60
+
+    car1 = Car(position=200, init_speed=100)
+    car2 = Car(position=240)
+    assert car2.speed == 60 != car1.speed
+    car1.brake_if_needed(car2)
+    assert car1.speed == car2.speed == 60
+
+def test_car_slows_if_over_desired_speed():
+    car1 = Car(init_speed=100, desired_speed=60)
+    car2 = Car(position=900)
+    with mock.patch("random.random", return_value=1):
+        # disable random braking
+        car1.brake_if_needed(car2)
+    assert car1.speed == 60
+
+    with mock.patch("random.random", return_value=0.05):
+        # disable random braking
+        car1.brake_if_needed(car2)
+    assert car1.speed == 58
+
+    car1.speed = 100
+    car1.accelerate()
+    assert car1.speed == 60
+
+def test_car_decelerate():
+    car = Car()
+    assert car.speed == 60
+    car.decelerate()
+    assert car.speed == 58
+    car.speed = 1
+    car.decelerate()
+    assert car.speed == 0
+
+def test_step():
+    # Stop if collision imminent
+    car1 = Car(position=200, init_speed=100)
+    car2 = Car(position=210)
+    car1.step(car2)
+    assert car1.speed == 0
+
+    car1 = Car(position=200, init_speed=100)
+    car2 = Car(position=240)
+    car1.step(car2)
+    assert car1.speed == car2.speed
+
+    car1 = Car(position=200, init_speed=100)
+    car2 = Car(position=400)
+    car1.step(car2)
+    assert car1.speed == 102
+
+
+
+#Add road with looping zero
