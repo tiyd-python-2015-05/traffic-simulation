9a4d6909678acab9faaf323b386bcc5fe1b1a3fc
Tripp Shealy <tripp.shealy@gmail.com>
2015-05-25 12:39:43 -0400

    Hard more complete and graphed in ipython notebook.

--- hard_mode/car_hard.py
+++ hard_mode/car_hard.py
@@ -23,10 +23,10 @@ class Car:
         """Make the self.speed the same speed as car in front"""
         # self.speed = other.speed  <- can't do this beacause you get tailgaters that never leave
         if ((other.location - 5) - self.location) < 0:
-            if (((other.location + 995) - self.location) - 2) < 0:
+            if (((other.location + 6995) - self.location) - 2) < 0:
                 self.speed = 0
             else:
-                self.speed = (((other.location + 995) - self.location) - 2)
+                self.speed = (((other.location + 6995) - self.location) - 2)
 
         else:
             if (((other.location - 5) - self.location) - 2) < 0:
@@ -38,7 +38,7 @@ class Car:
         """Returns true if other car distance is less than speed of current car."""
         # if other location is is greater than 1000 needs to know what to do
         if ((other.location - 5) - self.location) < 0:
-            if other.location + 995 - self.location < self.speed + 3:
+            if other.location + 6995 - self.location < self.speed + 3:
                 return True
             else:
                 return False
@@ -49,13 +49,27 @@ class Car:
 
     def set_decelerate_chance(self):
         """For hard mode. Changes based on road curve."""
-        pass
+        if 0 < self.location <= 1000:
+            self.decelerate_chance = 1
+        elif 1001 < self.location <= 2000:
+            self.decelerate_chance = 1.4
+        elif 2001 <self.location <= 3000:
+            self.decelerate_chance = 1
+        elif 3001 <self.location <= 4000:
+            self.decelerate_chance = 2
+        elif 4001 <self.location <= 5000:
+            self.decelerate_chance = 1
+        elif 5001 <self.location <= 6000:
+            self.decelerate_chance = 1.2
+        elif 6001 <self.location <= 7000:
+            self.decelerate_chance = 1
 
     def set_speed(self, other):
         """Sets car speed based on self.location and other.location"""
         if self.calculate_slow_down(other):
             self.slow_down(other)
         else:
+            self.set_decelerate_chance()
             if random.random() <= (.1 * self.decelerate_chance):
                 self.decelerate()
             else:
