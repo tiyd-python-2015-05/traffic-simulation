03d5441c2a437cb02b9b7f9d294a520c30155119
Benjamin Phillips <benred42@gmail.com>
2015-05-25 23:34:49 -0400

    Added docstrings to traffic_sim.py

--- traffic_sim.py
+++ traffic_sim.py
@@ -11,6 +11,8 @@ class Car:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def accelerate(self, speed, accel_chance):
+        """Function has a 10% to reduce car's speed; otherwise increases car's speed by 2 up to top speed.
+        """
         if random.random() < (0.1 * accel_chance):
             if speed - 2 < 0:
                 return 0
@@ -25,6 +27,10 @@ class Car:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def set_speed(self, distance, your_speed, accel_chance=1):
+        """Calls accelerate method to set a new speed for the car.  It then checks to see if that new speed is greater
+        than the current distance to the car in front of it.  If it is not, it returns the new speed. Otherwise, it
+        sets the new speed to that distance.
+        """
         new_speed = self.accelerate(your_speed, accel_chance)
         if distance <= new_speed:
             return distance
@@ -37,6 +43,7 @@ class Car:
 
 
 class Aggressive(Car):
+    # Has a higher top speed and acceleration, as well as a lower chance to decelerate than parent class.
     def __init__(self):
         super().__init__()
         self.top_speed = 38.9
@@ -62,6 +69,8 @@ class Aggressive(Car):
 
 
 class Commercial(Car):
+    # Has a lower top speed and acceleration, a larger size, and requires
+    # twice the distance from the next car than the parent class.
     def __init__(self):
         super().__init__()
         self.size = 25
@@ -91,16 +100,24 @@ class Simulation:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def initial_positions(self):
+        """Resets the initial positions of all cars in the array so they are equally spaced on the road"""
         self.values[0][0] = np.linspace(5, (self.road-5), len(self.cars))
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def initial_speeds(self):
+        """Resets the initial speeds of all cars to zero in the array"""
         self.values[1][0] = np.zeros(len(self.cars))
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def find_distance(self, time, car, other_car):
+        """Finds the distance from the current car to the one in front of it (assumes the position of the car is that
+        of its front bumper.  Subtracts the front car's size from its position to first find the location of its rear
+        bumper.  The if-statement is included to ensure that the car closest to the end of the simulated road sees the
+        car closest to the beginning of the road as "in front" of it (since cars exiting the road re-enter at the
+        beginning.
+        """
         rear_bumper = self.values[0][time-1][other_car] - self.cars[other_car].size
         distance = rear_bumper - self.values[0][time-1][car]
         if distance < 0:
@@ -110,6 +127,9 @@ class Simulation:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def get_speed(self, distance, time, car):
+        """Finds the new speed of the car at the current time point.  Changes the accel_chance argument of the car's
+        set_speed method based on the car's current position (as cars are more likely to slow down on certain
+        stretches of road than others."""
         position = self.values[0][time-1][car]
         your_speed = self.values[1][time-1][car]
         if position <= 1000:
@@ -130,6 +150,13 @@ class Simulation:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
     def run_simulation(self):
+        """Starts by setting the positions and speeds of the cars to their initial values. Then iterates through each
+        time point.  At each time point, iterates through the cars and finds their new speeds.  Then adds those speeds
+         to the cars' positions at the previous time point to determine their new positions. Then checks to see if any
+         of those positions are beyond the length of the road and if they are subtracts the length of the road from
+          those positions (effectively moving those cars back around to the beginning of the road).  Returns all but
+          the first 61 time points (essentially, the simulation runs for a minute first to set up a randomized traffic
+           flow)."""
         self.initial_positions()
         self.initial_speeds()
         for time in range(1, (len(self.values[0]))):
@@ -153,6 +180,9 @@ class Simulation:
 
 
 def create_cars(number, one_type=True):
+    """Creates either a list of Car objects of the same type or a list of Car objects where each object has different
+    percent chance to be the parent class (75%), the Aggressive subclass (10%), or the Commercial subclass (15%).
+    """
     if one_type:
         car_list = [Car() for _ in range(number)]
     else:
